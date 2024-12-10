39bc0e9b8edc8d13b3bc7d00b2081d571c2707d6
kelsssss <114365356+kelsssss@users.noreply.github.com>
2024-12-10 17:15:41 +0300

    update

---
+++ app/src/main/java/com/example/beatvault/data/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.example.beatvault.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+//    private const val BASE_URL = "https://example"
+//    private val retrofit: Retrofit by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//}
+//ГОТОВО на будущее
+
+}
